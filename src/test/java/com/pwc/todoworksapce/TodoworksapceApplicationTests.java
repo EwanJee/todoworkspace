02a5a2027b1a49d552861407a267a29e83774df3
Ewan Jee <830jiforcareer@gmail.com>
2025-11-11 20:13:08 +0900

    init test

---
+++ src/test/java/com/pwc/todoworksapce/TodoworksapceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwc.todoworksapce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoworksapceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
