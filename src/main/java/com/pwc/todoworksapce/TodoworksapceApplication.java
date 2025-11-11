89ae01040cc77d84cb7b0c008e88cc922e936270
Ewan Jee <830jiforcareer@gmail.com>
2025-11-11 20:12:53 +0900

    init

---
+++ src/main/java/com/pwc/todoworksapce/TodoworksapceApplication.java
@@ -0,0 +1,13 @@
+package com.pwc.todoworksapce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoworksapceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoworksapceApplication.class, args);
+	}
+
+}
